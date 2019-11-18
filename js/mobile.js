6f72379b4fe4497897e1b7c0e2f962a39a180426
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-11-17 22:44:16 -0400

    Add script to handle transition on nav links

--- js/mobile.js
+++ js/mobile.js
@@ -1,4 +1,16 @@
+const pathOfHamburgerIcon = "./assets/hamburger-menu.png";
+const pathOfCloseIcon = "./assets/close.png";
+
 function handleMobileMenu(){
-  let menuIcon = document.getElementsByClassName("nav__menu-icon");
-  menuIcon.style = "height: auto; padding 25px;";
+  let menuIcon = document.getElementsByClassName("nav__menu-icon")[0];
+  let menuContent = document.getElementsByClassName("nav__menu-content")[0];
+  if(menuIcon.src.match(/hamburger-menu/g)){
+    menuContent.style.height = "275px";
+    menuContent.style.padding = "25px";
+    menuIcon.src = pathOfCloseIcon;
+  }else{
+    menuContent.style.height = "0";
+    menuContent.style.padding = "0";
+    menuIcon.src = pathOfHamburgerIcon;
+  }
 }
