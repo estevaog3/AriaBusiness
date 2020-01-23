ae71d1b2e9ec09dbcbdad4fa88639607f8b817d0
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2020-01-23 11:50:08 -0400

    Add function to handle 'tabs-container' interaction

--- js/mobile.js
+++ js/mobile.js
@@ -13,5 +13,31 @@ function toggleMobileMenu(){
   }
 }
 
+let tabBodies = document.getElementsByClassName('tabs-container__tab-body');
+
+function handleTabInteraction(event){
+  if(tabBodies.length != navItems.length){
+    console.error("There must be a equal number of nav items and tab bodies");
+    return;
+  }
+  let selectedItem = parseInt(event.target.classList[0].slice(-1)); //the last character must be the item number
+  if(isNaN(selectedItem) || selectedItem < 0 
+  || selectedItem > navItems.length || selectedItem > tabBodies.length){
+    console.error("'selectedItem' is invalid:", selectedItem);
+    return;
+  }
+  for (let i = 0; i < navItems.length; i++){
+    navItems[i].classList.remove('tabs-container__nav-item--selected');
+    tabBodies[i].classList.remove('tabs-container__tab-body--selected');
+  }
+  tabBodies[selectedItem - 1].classList.add('tabs-container__tab-body--selected');
+  navItems[selectedItem - 1].classList.add('tabs-container__nav-item--selected');
+}
+
+let navItems = document.getElementsByClassName("tabs-container__nav-item");
+for (let i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener('click', handleTabInteraction);
+}
+
 let menuIcon = document.getElementsByClassName("nav__menu-icon")[0];
 menuIcon.addEventListener("click", toggleMobileMenu);
