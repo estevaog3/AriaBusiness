cbcc822d4a9e819956ce2190a21ed86e8a49f8bc
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2020-01-23 22:44:06 -0400

    Refactor and fix tab-container interaction handler
    The classes 'tab-container__nav-item-X' (where X is 1,2 or 3) is needed
    on the icons to allow the transition to work when the icon is clicked.

--- js/mobile.js
+++ js/mobile.js
@@ -13,31 +13,33 @@ function toggleMobileMenu(){
   }
 }
 
-let tabBodies = document.getElementsByClassName('tab-container__tab-body');
+let menuIcon = document.getElementsByClassName("nav__menu-icon")[0];
+menuIcon.addEventListener("click", toggleMobileMenu);
 
+let tabBodies = document.getElementsByClassName('tab-container__tab-body');
+let navItems = document.getElementsByClassName("tab-container__nav-item");
+let current = {
+  'tabBodySelected': tabBodies[0],
+  'navItemSelected': navItems[0]
+};
 function handleTabInteraction(event){
   if(tabBodies.length != navItems.length){
     console.error("There must be a equal number of nav items and tab bodies");
     return;
   }
   let selectedItem = parseInt(event.target.classList[0].slice(-1)); //the last character must be the item number
-  if(isNaN(selectedItem) || selectedItem < 0 
-  || selectedItem > navItems.length || selectedItem > tabBodies.length){
+  if(isNaN(selectedItem) || selectedItem < 0 || selectedItem > navItems.length){
     console.error("'selectedItem' is invalid:", selectedItem);
     return;
   }
-  for (let i = 0; i < navItems.length; i++){
-    navItems[i].classList.remove('tab-container__nav-item--selected');
-    tabBodies[i].classList.remove('tab-container__tab-body--selected');
-  }
-  tabBodies[selectedItem - 1].classList.add('tab-container__tab-body--selected');
+  current['navItemSelected'].classList.remove('tab-container__nav-item--selected');
+  current['tabBodySelected'].classList.remove('tab-container__tab-body--selected');
   navItems[selectedItem - 1].classList.add('tab-container__nav-item--selected');
+  tabBodies[selectedItem - 1].classList.add('tab-container__tab-body--selected');
+  current['navItemSelected'] = navItems[selectedItem - 1];
+  current['tabBodySelected'] = tabBodies[selectedItem - 1];
 }
 
-let navItems = document.getElementsByClassName("tab-container__nav-item");
 for (let i = 0; i < navItems.length; i++) {
   navItems[i].addEventListener('click', handleTabInteraction);
 }
-
-let menuIcon = document.getElementsByClassName("nav__menu-icon")[0];
-menuIcon.addEventListener("click", toggleMobileMenu);
