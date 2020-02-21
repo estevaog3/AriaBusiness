7f9ec5bd29f07a82de975863b283ef0a35382489
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2020-02-21 16:29:36 -0400

    Combine js/mobile.js js/desktop.js into one script: src/index.js

--- src/index.js
+++ src/index.js
@@ -88,3 +88,16 @@ let formHandler = ( () => {
     formMessages[forms[i].id] = document.querySelector(`[data-message-of='${forms[i].id}']`);
   }
 })();
+
+const scrollTopThreshold = 2;
+let nav = document.getElementsByClassName('nav')[0];
+
+function handleScroll(){
+  if(document.body.scrollTop > scrollTopThreshold || document.documentElement.scrollTop > scrollTopThreshold){
+    nav.classList.add('nav--on-scroll');
+  }else{
+    nav.classList.remove('nav--on-scroll');
+  }
+}
+
+window.onscroll = () => {handleScroll()};
